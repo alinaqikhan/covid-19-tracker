4334186b9d3dbf3bf58cc11159776ad02f587119
Ali Naqi Khan <alinaqikofficial@gmail.com>
2021-10-01 19:34:13 +0500

    Updated Chart component to display country based statistics

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
-export const Chart = () => {
+export const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
 
     const [dailyData, setDailyData] = useState([]);
 
@@ -38,10 +39,30 @@ export const Chart = () => {
         ) : null
       );
 
+    const barChart = (
+        confirmed ? (
+          <Bar 
+          data={{
+            labels: ['Infected', 'Recovered', 'Deaths'],
+            datasets: [{
+              label:'People',
+              backgroundColor: ['rgba(255, 0, 0, 0.5)', 'rgba(0, 255, 0, 0.5)','rgba(0, 0, 255, 0.5)'],
+              data: [confirmed.value, recovered.value, deaths.value]
+            }
+          ]
+          }}
+          options={{
+            legend: { display: false },
+            title: { display: true, text: `Current state in ${country}`}
+          }}
+          />
+        ): null
+      )
+
 
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country? barChart:lineChart}
         </div>
     )
 }
